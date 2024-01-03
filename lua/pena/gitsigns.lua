a38309afbcfb53e3fe5b729bcfc70a9b1ed8c908
hanif <mhanif.hasbullah@celcomdigi.com>
2024-01-03 11:19:08 +0800

    add git blame keymap

--- lua/pena/gitsigns.lua
+++ lua/pena/gitsigns.lua
@@ -47,6 +47,7 @@ gitsigns.setup {
   },
   on_attach = function(bufnr)
     vim.keymap.set('n', '<leader>hp', gitsigns.preview_hunk, { buffer = bufnr, desc = 'Preview git hunk' })
+    vim.keymap.set('n', '<leader>gb', gitsigns.blame_line, { buffer = bufnr, desc = 'Git blame' })
 
     -- don't override the built-in and fugitive keymaps
     local gs = package.loaded.gitsigns
