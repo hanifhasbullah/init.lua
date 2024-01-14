80535bcb9a7b1b4e0ba8aa54229f7ffa46b3ea08
hanif hasbullah <hcd@PMP.realtek>
2024-01-14 10:32:25 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,75 @@
+# Pena.nvim
+
+### Credits
+
+This project relies on the following resources. Special thanks to the respective authors and an AI:
+* [ThePrimeagen](https://github.com/ThePrimeagen/init.lua)
+* [kickstart.nvim](https://github.com/nvim-lua/kickstart.nvim)
+* [chrisatmachine](https://github.com/LunarVim/Neovim-from-scratch)
+* ChatGPT
+
+### Introduction
+
+A starting point for Neovim that is:
+
+* Small
+* Single-file (with examples of moving to multi-file)
+* Documented
+* Modular
+
+### Installation
+
+> **NOTE** 
+> [Backup](#FAQ) your previous configuration (if any exists)
+
+Requirements:
+* Make sure to review the readmes of the plugins if you are experiencing errors. In particular:
+  * [ripgrep](https://github.com/BurntSushi/ripgrep#installation) is required for multiple [telescope](https://github.com/nvim-telescope/telescope.nvim#suggested-dependencies) pickers.
+
+### Post Installation
+
+Start Neovim
+
+```sh
+nvim
+```
+
+The `Lazy` plugin manager will start automatically on the first run and install the configured plugins - as can be seen in the introduction video. After the installation is complete you can press `q` to close the `Lazy` UI and **you are ready to go**! Next time you run nvim `Lazy` will no longer show up.
+
+If you would prefer to hide this step and run the plugin sync from the command line, you can use:
+
+```sh
+nvim --headless "+Lazy! sync" +qa
+```
+
+### Recommended Steps
+
+[Fork](https://docs.github.com/en/get-started/quickstart/fork-a-repo) this repo (so that you have your own copy that you can modify) and then installing you can install to your machine using the methods above.
+
+### Configuration And Extension
+
+* Inside of your copy, feel free to modify any file you like! It's your copy!
+* Feel free to change any of the default options in `init.lua` to better suit your needs.
+* For adding plugins, there are 3 primary options:
+  * Add new configuration in `lua/custom/plugins/*` files, which will be auto sourced using `lazy.nvim` (uncomment the line importing the `custom/plugins` directory in the `init.lua` file to enable this)
+  * Modify `init.lua` with additional plugins.
+  * Include the `lua/kickstart/plugins/*` files in your configuration.
+
+You can also merge updates/changes from the repo back into your fork, to keep up-to-date with any changes for the default configuration.
+
+### FAQ
+
+* What should I do if I already have a pre-existing neovim configuration?
+  * You should back it up, then delete all files associated with it.
+  * This includes your existing init.lua and the neovim files in `~/.local` which can be deleted with `rm -rf ~/.local/share/nvim/`
+  * You may also want to look at the [migration guide for lazy.nvim](https://github.com/folke/lazy.nvim#-migration-guide)
+* Can I keep my existing configuration in parallel to kickstart?
+  * Yes! You can use [NVIM_APPNAME](https://neovim.io/doc/user/starting.html#%24NVIM_APPNAME)`=nvim-NAME` to maintain multiple configurations. For example you can install the kickstart configuration in `~/.config/nvim-kickstart` and create a script `~/bin/nvim-kickstart`:
+    ```
+    #!/bin/sh
+    exec env NVIM_APPNAME=nvim-kickstart nvim "$@"
+    ```
+    When you run Neovim with `nvim-kickstart` it will use the alternative config directory and the matching local directory: `~/.local/share/nvim-kickstart`. You can apply this approach to any Neovim distribution that you would like to try out.
+* What if I want to "uninstall" this configuration:
+  * See [lazy.nvim uninstall](https://github.com/folke/lazy.nvim#-uninstalling) information
+
---
+++ lua/pena/plugins.lua
@@ -0,0 +1,144 @@
+-- [[ Install `lazy.nvim` plugin manager ]]
+--    https://github.com/folke/lazy.nvim
+--    `:help lazy.nvim.txt` for more info
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  }
+end
+vim.opt.rtp:prepend(lazypath)
+
+local status_ok, lazy = pcall(require, 'lazy')
+if not status_ok then
+  return
+end
+
+lazy.setup({
+  "folke/lazy.nvim",
+  "nvim-lua/popup.nvim",
+
+  -- bbye
+  "moll/vim-bbye",
+
+  -- colorscheme
+  { 'rose-pine/neovim', name = 'rose-pine' },
+
+  -- cmp
+  {
+    -- Autocompletion
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+      -- Snippet Engine & its associated nvim-cmp source
+      'L3MON4D3/LuaSnip',
+      'saadparwaiz1/cmp_luasnip',
+
+      -- Buffer completion
+      'hrsh7th/cmp-buffer',
+
+      -- Path completion
+      'hrsh7th/cmp-path',
+
+      -- Adds LSP completion capabilities
+      'hrsh7th/cmp-nvim-lsp',
+
+      -- Adds a number of user-friendly snippets
+      'rafamadriz/friendly-snippets',
+    },
+  },
+
+  -- LSP
+  {
+    -- LSP Configuration & Plugins
+    'neovim/nvim-lspconfig',
+    dependencies = {
+      -- Automatically install LSPs to stdpath for neovim
+      'williamboman/mason.nvim',
+      'williamboman/mason-lspconfig.nvim',
+
+      -- Useful status updates for LSP
+      -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+      { 'j-hui/fidget.nvim', opts = {} },
+
+      -- Additional lua configuration, makes nvim stuff amazing!
+      'folke/neodev.nvim',
+
+      {'RRethy/vim-illuminate', commit = 'd6ca7f77eeaf61b3e6ce9f0e5a978d606df44298'}
+    },
+  },
+
+  -- Useful plugin to show you pending keybinds.
+  { 'folke/which-key.nvim', opts = {} },
+
+  -- Telescope Fuzzy Finder (files, lsp, etc)
+  {
+    'nvim-telescope/telescope.nvim',
+    branch = '0.1.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      -- Fuzzy Finder Algorithm which requires local dependencies to be built.
+      -- Only load if `make` is available. Make sure you have the system
+      -- requirements installed.
+      {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        -- NOTE: If you are having trouble with this installation,
+        --       refer to the README for telescope-fzf-native for more instructions.
+        build = 'make',
+        cond = function()
+          return vim.fn.executable 'make' == 1
+        end,
+      },
+    },
+  },
+
+  -- "gc" to comment visual regions/lines
+  { 'numToStr/Comment.nvim', opts = {}, lazy = false, },
+
+  -- Treesitter
+  { -- Highlight, edit, and navigate code
+    'nvim-treesitter/nvim-treesitter',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+    },
+    build = ':TSUpdate',
+  },
+
+  -- autopairs
+  { 'windwp/nvim-autopairs', event = "InsertEnter", opts = {}},
+
+  -- nvim-tree
+  { 'nvim-tree/nvim-tree.lua',
+    dependencies = {
+      'nvim-tree/nvim-web-devicons'
+    }
+  },
+
+  -- lualine
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    opts = {
+      options = {
+        icons_enabled = false,
+        theme = 'auto',
+        component_separators = '|',
+        section_separators = '',
+      },
+    },
+  },
+
+  -- buffer tab
+  {'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons'},
+
+  -- Terminal
+  {'akinsho/toggleterm.nvim', version = "*", config = true},
+
+  -- Git
+  { 'lewis6991/gitsigns.nvim' },
+},{})
+
